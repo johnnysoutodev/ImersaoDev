e4bbe1d58eecca0836740d2a475edb2f1b52aea3
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2022-09-07 16:07:34 -0300

    Create calculadora.js
    lógica da calculadora pra calcular a média de notas de um aluno

---
+++ src/aula-01/js/calculadora.js
@@ -0,0 +1,20 @@
+// calculadora.js
+
+let nome = 'Guilherme';
+let notaDoPrimeiroBimestre = 9;
+let notaDoSegundoBimestre = 7;
+let notaDoTerceiroBimestre = 4;
+let notaDoQuartoBimestre = 3;
+let notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4;
+
+let notaFixada = notaFinal.toFixed(2);
+
+console.log("Bem vindo " + nome);
+console.log(notaFixada);
+
+// Revisão
+
+/*
+Variaveis, Strings, console.log, toFixed, operações matematicas e concatenação foram faladas nessa aula 01 da imersão dev da alura.
+
+*/
